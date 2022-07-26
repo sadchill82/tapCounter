eb55a7c75d52684eed0f2d17c8a72f7dba98fb71
sadchill82 <nurmuhametoffislam@yandex.ru>
2022-07-26 23:01:54 +0300

    Initial Commit

---
+++ NewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewApp
+//
+//  Created by Ислам Нурмухаметов on 26.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
